79487b061b7b68328caf076a2c7db371abf2c6f3
Paul Gregoire <mondain@gmail.com>
2016-06-01 09:47:28 -0400

    Added fake cert and ssl files to help debug rtmps

--- src/main/java/org/red5/client/net/rtmps/RTMPSClient.java
+++ src/main/java/org/red5/client/net/rtmps/RTMPSClient.java
@@ -18,25 +18,10 @@
 
 package org.red5.client.net.rtmps;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.NotActiveException;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
-import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.future.IoFuture;
 import org.apache.mina.core.future.IoFutureListener;
 import org.apache.mina.core.session.IoSession;
@@ -44,11 +29,18 @@ import org.apache.mina.filter.ssl.SslFilter;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 import org.red5.client.net.rtmp.RTMPClient;
 import org.red5.client.net.rtmp.RTMPMinaIoHandler;
+import org.red5.client.net.ssl.BogusSslContextFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * RTMPS client object
+ * 
+ * <pre>
+ * var nc:NetConnection = new NetConnection();
+ * nc.proxyType = "best";
+ * nc.connect("rtmps:\\localhost\app");
+ * </pre>
  * 
  * @author The Red5 Project
  * @author Paul Gregoire (mondain@gmail.com)
@@ -65,11 +57,6 @@ public class RTMPSClient extends RTMPClient {
      * Password for accessing the keystore.
      */
     private char[] password;
-
-    /**
-     * Stores the keystore file bytes.
-     */
-    private byte[] keystore;
 
     /**
      * The keystore type, valid options are JKS and PKCS12
@@ -120,50 +107,6 @@ public class RTMPSClient extends RTMPClient {
     }
 
     /**
-     * Set keystore data from a file.
-     * 
-     * @param path
-     *            contains keystore
-     */
-    public void setKeystoreFile(String path) {
-        FileInputStream fis = null;
-        try {
-            File file = new File(path);
-            if (file.exists()) {
-                fis = new FileInputStream(file);
-                FileChannel fc = fis.getChannel();
-                ByteBuffer fb = ByteBuffer.allocate(Long.valueOf(file.length()).intValue());
-                fc.read(fb);
-                fb.flip();
-                keystore = IoBuffer.wrap(fb).array();
-            } else {
-                log.warn("Keystore file does not exist: {}", path);
-            }
-            file = null;
-        } catch (Exception e) {
-            log.warn("Error setting keystore data", e);
-        } finally {
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-    }
-
-    /**
-     * Set keystore data from a file.
-     * 
-     * @param arr
-     *            keystore bytes
-     */
-    public void setKeystoreBytes(byte[] arr) {
-        keystore = new byte[arr.length];
-        System.arraycopy(arr, 0, keystore, 0, arr.length);
-    }
-
-    /**
      * Set the key store type, JKS or PKCS12.
      * 
      * @param keyStoreType
@@ -174,31 +117,12 @@ public class RTMPSClient extends RTMPClient {
 
     private class RTMPSClientIoHandler extends RTMPMinaIoHandler {
 
-        // Create a trust manager that does not validate certificate chains
-        private final TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-
-            public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-            }
-
-            public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-            }
-        } };
-
         /** {@inheritDoc} */
         @Override
         public void sessionOpened(IoSession session) throws Exception {
-            if (password == null || keystore == null) {
-                throw new NotActiveException("Keystore or password are null");
-            }
             // START OF NATIVE SSL STUFF
-            //install the all-trusting trust manager
-            SSLContext context = SSLContext.getInstance("SSL");
-            context.init(null, trustAllCerts, new SecureRandom());
-            //create the ssl filter using client mode
-            SslFilter sslFilter = new SslFilter(context);
+            SSLContext sslContext = BogusSslContextFactory.getInstance(false);
+            SslFilter sslFilter = new SslFilter(sslContext);
             sslFilter.setUseClientMode(true);
             if (sslFilter != null) {
                 session.getFilterChain().addFirst("sslFilter", sslFilter);
@@ -217,29 +141,6 @@ public class RTMPSClient extends RTMPClient {
             }
             //if there are any errors using ssl, kill the session
             session.close(true);
-        }
-
-        /**
-         * Returns a KeyStore.
-         * 
-         * @return KeyStore
-         * @throws IOException
-         * @throws CertificateException
-         * @throws NoSuchAlgorithmException
-         * @throws KeyStoreException
-         */
-        @SuppressWarnings("unused")
-        private KeyStore getKeyStore() throws NoSuchAlgorithmException, CertificateException, IOException, KeyStoreException {
-            // Sun's default kind of key store
-            KeyStore ks = KeyStore.getInstance(keyStoreType);
-            // For security, every key store is encrypted with a
-            // pass phrase that must be provided before we can load
-            // it from disk. The pass phrase is stored as a char[] array
-            // so it can be wiped from memory quickly rather than
-            // waiting for a garbage collector. Of course using a string
-            // literal here completely defeats that purpose.
-            ks.load(new ByteArrayInputStream(keystore), password);
-            return ks;
         }
 
     }
--- src/test/java/org/red5/client/net/rtmps/RTMPSClientTest.java
+++ src/test/java/org/red5/client/net/rtmps/RTMPSClientTest.java
@@ -2,7 +2,7 @@ package org.red5.client.net.rtmps;
 
 import org.junit.Test;
 import org.red5.client.net.rtmp.ClientExceptionHandler;
-import org.red5.client.net.rtmp.RTMPClient;
+import org.red5.client.util.PropertiesReader;
 import org.red5.io.utils.ObjectMap;
 import org.red5.server.api.service.IPendingServiceCall;
 import org.red5.server.api.service.IPendingServiceCallback;
@@ -12,7 +12,7 @@ public class RTMPSClientTest {
     // https://github.com/Red5/red5-client/pull/31
     @Test
     public void test31() throws InterruptedException {
-        final RTMPClient client = new RTMPClient();
+        final RTMPSClient client = new RTMPSClient();
         client.setConnectionClosedHandler(new Runnable() {
             public void run() {
                 System.out.println("Connection closed");
@@ -26,7 +26,7 @@ public class RTMPSClientTest {
         });
         Thread t = new Thread(new Runnable(){
             public void run() {
-                client.connect("localhost", 8443, "live", new IPendingServiceCallback() {
+                client.connect(PropertiesReader.getProperty("rtmps.server"), Integer.valueOf(PropertiesReader.getProperty("rtmps.port")), PropertiesReader.getProperty("rtmps.app"), new IPendingServiceCallback() {
                     @Override
                     public void resultReceived(IPendingServiceCall result) {
                         System.out.println("resultReceived: " + result);
