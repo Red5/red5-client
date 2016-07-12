a8e8540346d446d8a9111effb3a561ed3ec9f774
Maxim Solodovnik <solomax666@gmail.com>
2016-07-12 10:13:32 +0600

    Deprecated call was removed

--- src/main/java/org/red5/client/net/rtmps/RTMPSClient.java
+++ src/main/java/org/red5/client/net/rtmps/RTMPSClient.java
@@ -134,7 +134,6 @@ public class RTMPSClient extends RTMPClient {
         }
 
         /** {@inheritDoc} */
-        @SuppressWarnings("deprecation")
         @Override
         public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
             log.warn("Exception caught {}", cause.getMessage());
@@ -142,7 +141,7 @@ public class RTMPSClient extends RTMPClient {
                 log.error("Exception detail", cause);
             }
             //if there are any errors using ssl, kill the session
-            session.close(true);
+            session.closeNow();
         }
 
     }
